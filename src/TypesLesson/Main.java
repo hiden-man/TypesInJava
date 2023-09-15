9d8d0f6999a0b28e7c191704ea0b24b4c5a858f8
hiden-man <elderlutiquie@gmail.com>
2023-09-15 13:04:31 +0300

    first program in java

---
+++ src/TypesLesson/Main.java
@@ -0,0 +1,27 @@
+package TypesLesson;
+
+public class Main {
+	public static void main(String[] args) {
+		TypesMethod();
+	}
+	public static void TypesMethod() {
+//----------------------------------------------------------
+		byte num1 = 127;
+		short num2 = 12345;
+		int num3 = 1893333333;
+		long num4 = 3929489584848585754l;
+		double num5 = 30404.0334;
+		float num6 = 3454.44565f;
+		char sym = 'R';
+		String str = "super";
+//----------------------------------------------------------
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		System.out.println(num6);
+		System.out.println(sym);
+		System.out.println(str);
+	}
+}
